4db82bcee43ea31102887bd5afbda23017cf7541
Tiemi Fernandes <tiemi.bfnagazawa@hotmail.com>
2023-12-05 19:41:49 -0400

    feat: shows list of saved memories on HomePage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,34 @@
-import React from 'react'
-import './globals.css'
+import { Copyright } from "@/components/Copyright";
+import { Hero } from "@/components/Hero";
+import { Profile } from "@/components/Profile";
+import { SignIn } from "@/components/SignIn";
 import {
-  Roboto_Flex as Roboto,
   Bai_Jamjuree as BaiJamjuree,
-} from 'next/font/google'
-import { cookies } from 'next/headers'
-import { Hero } from '@/components/Hero'
-import { Profile } from '@/components/Profile'
-import { SignIn } from '@/components/SignIn'
-import { Copyright } from '@/components/Copyright'
+  Roboto_Flex as Roboto,
+} from "next/font/google";
+import { cookies } from "next/headers";
+import React from "react";
+import "./globals.css";
 
-const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
+const roboto = Roboto({ subsets: ["latin"], variable: "--font-roboto" });
 const baiJamJuree = BaiJamjuree({
-  subsets: ['latin'],
-  weight: '700',
-  variable: '--font-bai-jamjuree',
-})
+  subsets: ["latin"],
+  weight: "700",
+  variable: "--font-bai-jamjuree",
+});
 
 export const metadata = {
-  title: 'Time Capsule',
+  title: "Time Capsule",
   description:
-    'Uma cápsula do tempo construída com React, NextJS, Tailwind e Typescript ',
-}
+    "Uma cápsula do tempo construída com React, NextJS, Tailwind e Typescript ",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-  const isAuthenticated = cookies().has('token')
+  const isAuthenticated = cookies().has("token");
   return (
     <html lang="en">
       <body
@@ -37,7 +37,7 @@ export default function RootLayout({
         <main className="grid min-h-screen grid-cols-2">
           <div className="br-[url(../assets/bg-stars.svg)] relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-cover px-28 py-16">
             <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
-            <div className="absolute bottom-0 right-2 top-0 w-2  bg-stripes" />
+            <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
 
             {isAuthenticated ? <Profile /> : <SignIn />}
 
@@ -45,11 +45,11 @@ export default function RootLayout({
             <Copyright />
           </div>
 
-          <div className="br-[url(../assets/bg-stars.svg)] flex flex-col bg-cover p-16">
+          <div className="br-[url(../assets/bg-stars.svg)] flex max-h-screen flex-col overflow-y-scroll bg-cover">
             {children}
           </div>
         </main>
       </body>
     </html>
-  )
+  );
 }

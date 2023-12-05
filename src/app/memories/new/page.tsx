e949f9aa2752f80c68696b3e9d952c6bcf7c7ba6
Tiemi Fernandes <tiemi.bfnagazawa@hotmail.com>
2023-12-04 20:16:31 -0400

    feat: adds media preview on new-memories page

--- src/app/memories/new/page.tsx
+++ src/app/memories/new/page.tsx
@@ -1,24 +1,25 @@
+import { MediaPicker } from '@/components/MediaPicker'
 import { Camera, ChevronLeft } from 'lucide-react'
 import Link from 'next/link'
 
 export default function NewMemory() {
   return (
-    <div className="flex flex-1 flex-col gap-4">
+    <div className="flex flex-col flex-1 gap-4">
       <Link
         href=""
         className="flex items-center gap-1 text-sm text-gray-200 hover:text-gray-100"
       >
-        <ChevronLeft className="h-4 w-4" />
+        <ChevronLeft className="w-4 h-4" />
         voltar à timeline
       </Link>
 
-      <form className="flex flex-1 flex-col gap-2">
+      <form className="flex flex-col flex-1 gap-2">
         <div className="flex items-center gap-4">
           <label
             htmlFor="media"
             className="flex cursor-pointer items-center gap-1.5 text-sm text-gray-200 hover:text-gray-100"
           >
-            <Camera className="h-4 w-4" />
+            <Camera className="w-4 h-4" />
             Anexar mídia
           </label>
           <label
@@ -30,16 +31,16 @@ export default function NewMemory() {
               name="isPublic"
               id="isPublic"
               value="true"
-              className="h-4 w-4 rounded border-gray-400 bg-gray-700 text-purple-500"
+              className="w-4 h-4 text-purple-500 bg-gray-700 border-gray-400 rounded"
             />
             Tornar memória pública
           </label>
         </div>
-        <input type="file" id="media" className="invisible h-0" />
+        <MediaPicker />
         <textarea
           name="content"
           spellCheck={false}
-          className="w-full flex-1 resize-none border-0 bg-transparent p-0 text-lg leading-relaxed text-gray-100 placeholder:text-gray-400 focus:ring-0"
+          className="flex-1 w-full p-0 text-lg leading-relaxed text-gray-100 bg-transparent border-0 resize-none placeholder:text-gray-400 focus:ring-0"
           placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
         />
       </form>

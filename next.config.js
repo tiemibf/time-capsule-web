b8239c667d77fb3ef3eae54db4cc689afdd91a80
Tiemi Fernandes <tiemi.bfnagazawa@hotmail.com>
2023-12-06 21:27:50 -0400

    fix: allows domain of pictures upload on next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com', '192.168.1.7'],
+    domains: ['avatars.githubusercontent.com', '127.0.0.1'],
   },
 }
 
